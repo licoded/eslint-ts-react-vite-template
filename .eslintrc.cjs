74c5ee0b1322611f6b971ee35df41735ad892e57
licoded <busy.li@foxmail.com>
2023-04-30 08:36:44 +0800

    eslint: set "no-shadow" from "error" to "warn"
    no-shadow: 变量覆盖, 内层声明变量覆盖外层声明的变量
    
    > 内层, 可以是for循环, 也可以是传入 map/setState 的函数的参数

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,5 +24,7 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     'import/extensions': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'warn',
   },
-}
+};
